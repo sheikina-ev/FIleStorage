cb3b366e5f288e24082f4be7c5537f509faf3534
lisa <liza.sheykina.2005@mail.ru>
2024-12-11 13:25:48 +0700

    Обновочка

---
+++ FIleStorage/Models/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIleStorage.Models
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Size { get; set; } // Размер как строка (например, "100 KB")
+        public string Path { get; set; }
+        public DateTime? CreatedAt { get; set; } // Nullable для случаев с null
+    }
+
+
+}
